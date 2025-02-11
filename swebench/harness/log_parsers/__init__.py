9931dca4be7f83ae7ad0424ca1c3936aac3c90d9
Kabir Khandpur <kabirk@live.com>
2025-02-11 23:00:12 +0000

    Add phpunit test log parser

--- swebench/harness/log_parsers/__init__.py
+++ swebench/harness/log_parsers/__init__.py
@@ -1,8 +1,10 @@
 from swebench.harness.log_parsers.javascript import MAP_REPO_TO_PARSER_JS
+from swebench.harness.log_parsers.php import MAP_REPO_TO_PARSER_PHP
 from swebench.harness.log_parsers.python import MAP_REPO_TO_PARSER_PY
 from swebench.harness.log_parsers.utils import get_eval_type
 
 MAP_REPO_TO_PARSER = {
     **MAP_REPO_TO_PARSER_JS,
+    **MAP_REPO_TO_PARSER_PHP,
     **MAP_REPO_TO_PARSER_PY,
 }
