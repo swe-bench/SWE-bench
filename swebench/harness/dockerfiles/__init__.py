8bf5221e7e82eb5ce829de9042d316df7278262e
Kabir Khandpur <kabirk@live.com>
2025-02-11 22:59:57 +0000

    Add php dockerfiles

--- swebench/harness/dockerfiles/__init__.py
+++ swebench/harness/dockerfiles/__init__.py
@@ -10,9 +10,15 @@ from swebench.harness.dockerfiles.python import (
     _DOCKERFILE_INSTANCE_PY,
 )
 
+from swebench.harness.dockerfiles.php import (
+    _DOCKERFILE_BASE_PHP,
+    _DOCKERFILE_INSTANCE_PHP,
+)
+
 _DOCKERFILE_BASE = {
     "py": _DOCKERFILE_BASE_PY,
     "js": _DOCKERFILE_BASE_JS,
+    "php": _DOCKERFILE_BASE_PHP,
 }
 
 _DOCKERFILE_ENV = {
@@ -23,25 +29,36 @@ _DOCKERFILE_ENV = {
 _DOCKERFILE_INSTANCE = {
     "py": _DOCKERFILE_INSTANCE_PY,
     "js": _DOCKERFILE_INSTANCE_JS,
+    "php": _DOCKERFILE_INSTANCE_PHP,
 }
 
-def get_dockerfile_base(platform, arch, language):
+def get_dockerfile_base(platform, arch, language, **kwargs):
     if arch == "arm64":
         conda_arch = "aarch64"
     else:
         conda_arch = arch
     return _DOCKERFILE_BASE[language].format(
         platform=platform,
-        conda_arch=conda_arch
+        conda_arch=conda_arch,
+        **kwargs
     )
 
 
 def get_dockerfile_env(platform, arch, language, **kwargs):
-    return _DOCKERFILE_ENV[language].format(
-        platform=platform,
-        arch=arch,
-        **kwargs
-    )
+    # Some languages do not have an environment Dockerfile. In those cases, the
+    # base Dockerfile is used as the environment Dockerfile.
+    if language in _DOCKERFILE_ENV:
+        return _DOCKERFILE_ENV[language].format(
+            platform=platform,
+            arch=arch,
+            **kwargs
+        )
+    else:
+        return _DOCKERFILE_BASE[language].format(
+            platform=platform,
+            arch=arch,
+            **kwargs
+        )
 
 
 def get_dockerfile_instance(platform, language, env_image_name):
